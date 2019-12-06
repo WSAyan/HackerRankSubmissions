2d26e7316d486d59be18ef53119a3f2652f112e5
Wahid Sadique Ayan <wsayan28@gmail.com>
2019-12-06 10:54:04 +0600

    Create SockMerchant.kt

---
+++ SockMerchant.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the sockMerchant function below.
+fun sockMerchant(n: Int, ar: Array<Int>):Int {
+
+return ar.groupingBy { it }.eachCount().filter { it.value > 1 }.map{ it.value - (it.value % 2) }.sum()/2
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
