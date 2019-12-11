d5ed7d95253fc574b353b27e6873bff34a12ac54
Wahid Sadique Ayan <wsayan28@gmail.com>
2019-12-11 11:41:23 +0600

    Create JumpingOnTheClouds.kt
    https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D%5B%5D=warmup

---
+++ JumpingOnTheClouds.kt
@@ -0,0 +1,39 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the jumpingOnClouds function below.
+fun jumpingOnClouds(c: Array<Int>): Int {
+    var step = 1
+    for (i in 1 until c.size-1) {
+        if( c[i]==0 ) if( c[i-1]!=0 || c[i+1]!=0 ) step++ else c[i] = 1
+    }
+    return step
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val c = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = jumpingOnClouds(c)
+
+    println(result)
+}
